36e7317922b36efc42bad900e0e3881907141118
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-18 04:19:20 +0900

    Update abc251_a.zig

--- abc251_a.zig
+++ abc251_a.zig
@@ -1,3 +1,59 @@
+
+############################################################
+
+const std = @import("std");
+
+const stdout = std.io.getStdOut();
+var buffer: [1000000]u8 = undefined;
+
+const stdin = std.io.getStdIn();
+var buf = std.io.bufferedReader(stdin.reader());
+var stdin_reader = buf.reader();
+
+var allocator: std.mem.Allocator = undefined;
+
+fn gets() []const u8 {
+    const line = stdin_reader.readUntilDelimiterOrEof(&buffer, '\n') catch unreachable;
+    return allocator.dupe(u8, line.?) catch unreachable;
+}
+
+fn geti() i64 {
+    return std.fmt.parseInt(i64, gets(), 10) catch unreachable;
+}
+
+fn getu() u64 {
+    return std.fmt.parseInt(u64, gets(), 10) catch unreachable;
+}
+
+fn getvecu() []const u64 {
+    var vecu = std.ArrayList(u64).init(allocator);
+    var it = std.mem.split(u8, gets(), " ");
+    var v = it.next();
+    while (v != null) : (v = it.next()) {
+        const val = std.fmt.parseInt(u64, v.?, 10) catch unreachable;
+        vecu.append(val) catch unreachable;
+    }
+    return vecu.items;
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    allocator = arena.allocator();
+    const s = gets();
+    const n = s.len;
+    if (n == 1) {
+    try stdout.writer().print("{s}{s}{s}{s}{s}{s}\n", .{ s,s,s,s,s,s });
+    } else if (n == 2) {
+    try stdout.writer().print("{s}{s}{s}\n", .{ s,s,s });
+    } else if (n == 3) {
+    try stdout.writer().print("{s}{s}\n", .{ s,s });
+    }
+}
+
+
+############################################################
+
 const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const stdin = std.io.getStdIn().reader();
@@ -445,3 +501,5 @@ pub fn main() !void {
 
   try stdout.print("{s}\n", .{result});
 }
+
+############################################################
