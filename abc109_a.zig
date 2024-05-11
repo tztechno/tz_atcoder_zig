036c03e1958299d96a2660e5aa946e9288277c63
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-11 17:22:35 +0900

    Create abc109_a.zig

---
+++ abc109_a.zig
@@ -0,0 +1,60 @@
+abc109_a.zig
+#############################################
+#############################################
+#############################################
+#############################################
+const std = @import("std");
+
+const stdout = std.io.getStdOut();
+var buffer: [1000000]u8 = undefined;
+
+const stdin = std.io.getStdIn();
+var buf = std.io.bufferedReader(stdin.reader());
+var stdin_reader = buf.reader();
+
+var allocator: std.mem.Allocator = undefined;
+
+fn gets() []const u8 {
+    const line = stdin_reader.readUntilDelimiterOrEof(&buffer, '\n') catch unreachable;
+    return allocator.dupe(u8, line.?) catch unreachable;
+}
+
+fn geti() i64 {
+    return std.fmt.parseInt(i64, gets(), 10) catch unreachable;
+}
+
+fn getu() u64 {
+    return std.fmt.parseInt(u64, gets(), 10) catch unreachable;
+}
+
+fn getvecu() []const i64 {
+    var vecu = std.ArrayList(i64).init(allocator);
+    var it = std.mem.split(u8, gets(), " ");
+    var v = it.next();
+    while (v != null) : (v = it.next()) {
+        const val = std.fmt.parseInt(i64, v.?, 10) catch unreachable;
+        vecu.append(val) catch unreachable;
+    }
+    return vecu.items;
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    allocator = arena.allocator();
+    const ab = getvecu();
+    const a = ab[0];
+    const b = ab[1];
+    const c = a * b;
+    
+    const ans = if (@rem(c, 2) == 1) "Yes" else "No";
+    try std.io.getStdOut().writer().print("{s}\n", .{ans});
+}
+#############################################
+[python]
+a,b=map(int,input().split())
+if (a*b)%2==1:
+    print('Yes')
+else:
+    print('No')
+#############################################
